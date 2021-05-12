3e777f0e765b52153c001d7ec11ac6792d0a5d70
SADmammoth <lo.ma200018@gmail.com>
2021-05-12 21:58:54 +0300

    feat: doc page styles

--- src/pagesContent/DocumentContentPage/DocumentContentPage.jsx
+++ src/pagesContent/DocumentContentPage/DocumentContentPage.jsx
@@ -4,6 +4,12 @@ import Blocks from 'editorjs-blocks-react-renderer';
 import PropTypes from 'prop-types';
 import { useTheme, createUseStyles } from 'react-jss';
 
+import Sidebar from '../../components/generic/Sidebar';
+import Text from '../../components/generic/Text';
+import GridLayout from '../../components/generic/layouts/GridLayout';
+import Skip from '../../components/generic/layouts/GridLayout/Skip';
+import StackLayout from '../../components/generic/layouts/StackLayout';
+
 import styles from './DocumentContentPage.styles';
 
 const useStyles = createUseStyles(styles);
@@ -12,7 +18,24 @@ function DocumentContentPage({ initialValues }) {
   const theme = useTheme();
   const classes = useStyles(theme);
   console.log(initialValues);
-  return <Blocks data={initialValues.content || { blocks: [] }} />;
+  return (
+    <GridLayout className={classes.content}>
+      <Sidebar column={3} className={classes.paddingTop}>
+        <Text type="common">{initialValues.abstract}</Text>
+      </Sidebar>
+      <Skip column={1} />
+      <StackLayout
+        column={5}
+        className={classes.doc}
+        orientation="vertical"
+        className={classes.paddingTop}>
+        <Text type="h1">{initialValues.title}</Text>
+        <div className={classes.blocks}>
+          <Blocks data={initialValues.content || { blocks: [] }} />
+        </div>
+      </StackLayout>
+    </GridLayout>
+  );
 }
 
 DocumentContentPage.propTypes = {};
--- src/pagesContent/DocumentContentPage/DocumentContentPage.styles.js
+++ src/pagesContent/DocumentContentPage/DocumentContentPage.styles.js
@@ -1,3 +1,10 @@
-const styles = {};
+const styles = {
+  content: {
+    height: '100%',
+  },
+  paddingTop: {
+    paddingTop: (theme) => theme.spaces.small,
+  },
+};
 
 export default styles;
